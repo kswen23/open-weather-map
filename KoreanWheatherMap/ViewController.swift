96bdfec664f4640b0557ea97d9fdb8d3fea902bc
김성원 <kswen0203@gmail.com>
2022-01-26 18:13:20 +0900

    podinit

---
+++ KoreanWheatherMap/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  KoreanWheatherMap
+//
+//  Created by 김성원 on 2022/01/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
